aaf63677dba26d1a0852dc0983afeaca6b4d98af
vhtiengiang177 <45086503+vhtiengiang177@users.noreply.github.com>
2020-05-25 15:35:38 +0700

    12

--- frmTest.Designer.cs
+++ frmTest.Designer.cs
@@ -34,7 +34,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(197, 74);
+            this.button1.Location = new System.Drawing.Point(11, 11);
             this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(200, 80);
