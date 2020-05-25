63dcc7036d24bc4999c879544ee54ea87d95114e
giang702 <giang160220@gmail.com>
2020-05-25 15:15:54 +0700

    1

--- frmTest.Designer.cs
+++ frmTest.Designer.cs
@@ -34,7 +34,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(165, 113);
+            this.button1.Location = new System.Drawing.Point(219, 67);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(267, 98);
             this.button1.TabIndex = 0;
