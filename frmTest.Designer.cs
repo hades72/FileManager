464506a84291f0462c79f5ee0dda7dc253f6861c
giang702 <giang160220@gmail.com>
2020-05-25 15:15:28 +0700

    0

--- frmTest.Designer.cs
+++ frmTest.Designer.cs
@@ -34,7 +34,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(96, 214);
+            this.button1.Location = new System.Drawing.Point(165, 113);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(267, 98);
             this.button1.TabIndex = 0;
@@ -43,7 +43,7 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(493, 188);
+            this.button2.Location = new System.Drawing.Point(533, 228);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(75, 23);
             this.button2.TabIndex = 1;
